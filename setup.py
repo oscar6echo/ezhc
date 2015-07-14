88d2bd929fa94b6c2f57fb2619c192e6108e9ee9
oscar6echo <olivier.borderies@gmail.com>
2015-07-14 22:43:12 +0200

    clean

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+# cf. http://peterdowns.com/posts/first-time-with-pypi.html
+# cf. https://tom-christie.github.io/articles/pypi/
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'ezhc',
+  packages = ['ezhc'],
+  version = '0.4.2',
+  description = 'easy Highcharts, dynamic plots from pandas dataframes in the IPython notebook',
+  author = 'oscar6echo',
+  author_email = 'olivier.borderies@gmail.com',
+  url = 'https://github.com/oscar6echo/ezhc', # use the URL to the github repo
+  download_url = 'https://github.com/oscar6echo/ezhc/tarball/0.4.2', # tag number at the end
+  keywords = ['Highcharts', 'pandas', 'notebook', 'javascript'], # arbitrary keywords
+  classifiers = [],
+)
