418ffbf513afb3808263b050628ab2a669ba9289
oscar6echo <olivier.borderies@gmail.com>
2015-07-14 23:28:07 +0200

    v0.4.5

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from setuptools import find_packages
 setup(
   name = 'ezhc',
   packages = ['ezhc'],
-  version = '0.4.4',
+  version = '0.4.5',
   description = 'easy Highcharts, dynamic plots from pandas dataframes in the IPython notebook',
   author = 'oscar6echo',
   author_email = 'olivier.borderies@gmail.com',
   url = 'https://github.com/oscar6echo/ezhc', # use the URL to the github repo
-  download_url = 'https://github.com/oscar6echo/ezhc/tarball/0.4.4', # tag number at the end
+  download_url = 'https://github.com/oscar6echo/ezhc/tarball/0.4.5', # tag number at the end
   keywords = ['Highcharts', 'pandas', 'notebook', 'javascript'], # arbitrary keywords
   license='MIT',
   classifiers = [ 'Development Status :: 4 - Beta',
