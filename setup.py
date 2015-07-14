989a1d8312708e10d5caad729474f0dc9b439549
oscar6echo <olivier.borderies@gmail.com>
2015-07-15 00:20:01 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 # cf. http://peterdowns.com/posts/first-time-with-pypi.html
 # cf. https://tom-christie.github.io/articles/pypi/
 
-from distutils.core import setup
-from setuptools import find_packages
+
+from setuptools import setup, find_packages
 
 with open('README.rst') as f:
     long_description = f.read()
@@ -24,4 +24,24 @@ setup(
                   'License :: OSI Approved :: MIT License',
                   'Programming Language :: Python :: 2.7'
   ],
+  include_package_data=True,
+  package_data={
+    'api':
+         ['api/hc_object.json',
+          'api/hc_option.json',
+          'api/hs_object.json',
+          'api/hs_option.json'
+         ],
+    'samples':
+        ['samples/df_bubble.csv',
+         'samples/df_heatmap.csv',
+         'samples/df_one_idx_one_col.csv',
+         'samples/df_one_idx_several_col_2.csv',
+         'samples/df_one_idx_several_col.csv',
+         'samples/df_one_idx_two_col.csv',
+         'samples/df_scatter.csv',
+         'samples/df_several_idx_one_col.csv',
+         'samples/df_two_idx_one_col.csv'
+        ]
+    },
 )
