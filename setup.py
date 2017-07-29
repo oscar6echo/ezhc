e99a2139f971aa76226821ddecd10a52a31495db
oscar6echo <olivier.borderies@gmail.com>
2017-07-29 20:17:14 +0200

    Correct typo

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
   classifiers = [ 'Development Status :: 4 - Beta',
                   'License :: OSI Approved :: MIT License',
                   'Programming Language :: Python :: 2.7',
-                  'Programming Language :: Python :: 3.5'
+                  'Programming Language :: Python :: 3.5',
                   'Programming Language :: Python :: 3.6'
   ],
   install_requires = [
