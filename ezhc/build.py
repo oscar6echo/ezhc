3b1ccb4ff54837eb9b225b6ad97f47b1f5d22f03
oscar6echo <olivier.borderies@gmail.com>
2017-12-24 23:20:04 +0100

    Add functions to build 3 graphs
    treemap, sunburnst: Recursive functions
    scatterplot: Add 2nd column to dataframe
    See demo notebook

--- ezhc/build.py
+++ ezhc/build.py
@@ -1,6 +1,9 @@
 
 import pandas as pd
 
+DEFAULT_COLORS = ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9",
+                  "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"]
+
 
 def series(df, *args, **kwargs):
     idx = df.index
@@ -72,7 +75,7 @@ def series_drilldown(df, colorByPoint=True, pointPlacement='on', *args, **kwargs
             d1 = {
                 'name': c,
                 'y': dfs[[co]].sum().values[0],
-                'drilldown': c+' - '+co if len(dfs) > 1 else None,
+                'drilldown': c + ' - ' + co if len(dfs) > 1 else None,
                 'pointPlacement': pointPlacement
             }
             if co in kwargs.get('color', []):
@@ -81,9 +84,9 @@ def series_drilldown(df, colorByPoint=True, pointPlacement='on', *args, **kwargs
 
             if len(dfs) > 1:
                 d2 = {
-                    'name': c+' - '+co,
+                    'name': c + ' - ' + co,
                     'data': [[str(ii[q]), vv[q]] for q in range(len(ii))],
-                    'id': c+' - '+co,
+                    'id': c + ' - ' + co,
                     'pointPlacement': pointPlacement
                 }
                 drilldownSeries.append(d2)
@@ -96,27 +99,36 @@ def series_drilldown(df, colorByPoint=True, pointPlacement='on', *args, **kwargs
     return series, drilldownSeries
 
 
-def series_scatter(df, *args, **kwargs):
+def series_scatter(df, color_column=None, title_column=None, *args, **kwargs):
     idx = df.index
     col = df.columns
     assert(isinstance(idx, pd.core.index.MultiIndex))
     assert(len(idx.levshape) == 2)
-    assert(len(col) == 1)
-    assert(df[col[0]].dtype.kind in 'iO')
+    assert(len(col) <= 2)
+    assert(df[color_column].dtype.kind in 'iO')
 
-    data = df.values.flatten()
+    if color_column == None:
+        assert(len(col) == 1)
+        color_column = col[0]
+    if title_column == None:
+        title_column = color_column
+
+    data = df[[color_column]].values.flatten()
     elmt = list(set(data))
     color = kwargs.get('color', {})
     series = []
 
     for e in elmt:
-        dfs = df[df.iloc[:, 0] == e]
+        dfs = df[df[color_column] == e]
         idx = dfs.index
+        names = list(dfs[title_column])
         series.append({'animation': False,
                        'name': e,
+                       'turboThreshold': 0,
                        'color': color.get(e, None),
-                       'data': [[idx[k][0], idx[k][1]] for k in range(len(dfs))],
-                    })
+                       'data': [{'x': idx[k][0], 'y': idx[k][1], 'name': str(names[k])}
+                                for k in range(len(dfs))],
+                       })
     return series
 
 
@@ -146,30 +158,6 @@ def series_bubble(df, *args, **kwargs):
     return series
 
 
-def series_treemap(df, *args, **kwargs):
-    idx = df.index
-    col = df.columns
-    assert(isinstance(idx, pd.core.index.MultiIndex))
-    assert(len(col) == 1)
-
-    data = df.values
-    elmt = list(set(data))
-    color = kwargs.get('color', {})
-    series = []
-
-    for e in elmt:
-        dfs = df[df.iloc[:, 0] == e]
-        idx = dfs.index
-        series.append({
-            'animation': False,
-            'name': e,
-            'color': color.get(e, None),
-            'data': [[idx[k][0], idx[k][1]] for k in range(len(dfs))],
-        })
-    return series
-    pass
-
-
 def series_heatmap(df, *args, **kwargs):
     idx = df.index
     col = df.columns
@@ -185,3 +173,102 @@ def series_heatmap(df, *args, **kwargs):
     res_col = list(df.index)
 
     return res_idx, res_col, res_data
+
+
+def series_tree(df,
+                set_total=False,
+                name_total='All',
+                set_color=False,
+                colors=DEFAULT_COLORS,
+                set_value=True,
+                precision=2):
+
+    class TreeBuilder:
+
+        def __init__(self,
+                     df,
+                     set_total,
+                     name_total,
+                     set_color,
+                     colors,
+                     set_value,
+                     precision):
+            self.df = df
+            self.set_color = set_color
+            self.colors = colors
+            self.set_value = set_value
+            self.precision = precision
+
+            self.points = []
+            if set_total:
+                point = {'id': '0',
+                         'name': name_total,
+                         }
+                if set_value:
+                    point['value'] = round(df.sum().sum(), precision)
+                self.points.append(point)
+                total = self.build(df, parent='0')
+            else:
+                total = self.build(df)
+
+        def build(self, dfxs, parent=None):
+            idx = dfxs.index
+            sum_value = 0
+
+            if isinstance(idx, pd.MultiIndex):
+                for k, name in enumerate(idx.levels[0]):
+                    point = {
+                        'name': name,
+                    }
+                    if parent:
+                        point['id'] = parent + '.' + str(k)
+                        point['parent'] = parent
+                    else:
+                        point['id'] = '0.' + str(k)
+                        if self.set_color:
+                            point['color'] = self.colors[k % len(self.colors)]
+
+                    df_sub = dfxs.xs(name)
+                    df_sub = df_sub.reset_index().set_index(df_sub.index.names)
+
+                    value = self.build(df_sub,
+                                       parent=point.get('id'))
+                    sum_value += value
+                    if self.set_value:
+                        point['value'] = round(value, self.precision)
+                    self.points.append(point)
+
+            elif isinstance(idx, pd.Index):
+
+                for k, name in enumerate(idx):
+                    value = dfxs.loc[name][0]
+                    sum_value += value
+
+                    point = {
+                        'name': name,
+                        'value': round(value, self.precision)
+                    }
+                    if parent:
+                        point['id'] = parent + '.' + str(k)
+                        point['parent'] = parent
+                    else:
+                        point['id'] = 'id_' + str(k)
+                        point['color'] = self.colors[k % len(self.colors)]
+
+                    self.points.append(point)
+
+            else:
+                raise Exception('Pandas Index Unknown Problem')
+
+            return sum_value
+
+    col = df.columns
+    assert(len(col) == 1)
+    tb = TreeBuilder(df,
+                     set_total,
+                     name_total,
+                     set_color,
+                     colors,
+                     set_value,
+                     precision)
+    return tb.points
